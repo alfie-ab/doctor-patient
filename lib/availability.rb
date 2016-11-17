8d609d2a3aad3618527608de6066d46d4120147b
Alfie <alfie_ab@hotmail.co.uk>
2016-11-17 17:38:36 +0000

    more spiking to retrieve times

--- lib/availability.rb
+++ lib/availability.rb
@@ -10,10 +10,18 @@ times = JSON.parse(file)
 #This is separate and refers exclusively to the command line input
 input = ARGV[0].to_i
 
+output = []
+
+times["availability_slots"].each do |a|
+  output << a["time"]
+end
+
+
 if input < 8
   puts "You must book after 08:00"
 elsif input > 15
   puts "You must book before 15:00"
 else
   puts "You booking has been confirmed"
+  puts output
 end
