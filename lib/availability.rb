a33571bb4ef2e8f8f8e7513ebb5b18f6ebbd6739
Alfie <alfie_ab@hotmail.co.uk>
2016-11-17 17:03:19 +0000

    spiking around with if/else

--- lib/availability.rb
+++ lib/availability.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'time'
 
+# This is taking the JSON file and parsing it to be used in ruby
 file = File.read('../times.json')
-json = JSON.parse(file)
+times = JSON.parse(file)
 
-case ARGV[0]
-when "12:40"
-  puts "You have been booked in for 12:40"
-  puts json
+#This is separate and refers exclusively to the command line input
+input = ARGV[0].to_i
+
+if input < 8
+  puts "You must book after 08:00"
+elsif input > 15
+  puts "You must book before 15:00"
 else
-  puts "That time is not available"
+  puts "You booking has been confirmed"
 end
